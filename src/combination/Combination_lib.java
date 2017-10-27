78500b02d99f4df2a07689db3e32f5ea0f6932dd
50616030 <50616030@50616030-PC>
2017-10-27 18:18:40 +0900

    combination課題

---
+++ src/combination/Combination_lib.java
@@ -0,0 +1,18 @@
+package combination;
+
+public class Combination_lib {
+  public int getFactorial(int n) {//n‚ÌŠKæ
+	  int f = 1,i;
+	  for(i = 1;i<=n;i++) {
+	    f=f*i;
+	  }
+		  
+	  //ŠKæŒvZ‚ğ“ü‚ê‚é
+	  return f;
+	
+	 
+		
+	  }
+	
+	  }
+
